13cb71d3ddc491b6aeaa2fb5cf80a8335e41423d
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2020-08-05 11:27:31 -0500

    Skeleton Assembly

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Styles/App.css';
+import Banner from './Components/Banner/Banner';
+import Calculator from './Components/Calculator/Calculator';
+import History from './Components/History/History'
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <Banner/>
+        <Calculator/>
+        <History/>
+        <Footer/>
       </header>
     </div>
   );
---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Banner(){
+    return(
+        <div className = 'banner'>
+            <h1>Banner</h1>
+        </div>
+    )
+}

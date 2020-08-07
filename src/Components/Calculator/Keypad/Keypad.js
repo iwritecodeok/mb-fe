7a9ca254f1885147dd3c21c3bf1c57cf3005892a
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2020-08-07 15:43:14 -0500

    Update Keypad.js

--- src/Components/Calculator/Keypad/Keypad.js
+++ src/Components/Calculator/Keypad/Keypad.js
@@ -15,23 +15,23 @@ export default function Keypad(props){
         </ButtonRow>
         {/* row 2 */}
         <ButtonRow>
-        <Button onButtonPress={props.onButtonPress}>9</Button>
-        <Button onButtonPress={props.onButtonPress}>8</Button>
         <Button onButtonPress={props.onButtonPress}>7</Button>
+        <Button onButtonPress={props.onButtonPress}>8</Button>
+        <Button onButtonPress={props.onButtonPress}>9</Button>
         <Button onButtonPress={props.onButtonPress}>*</Button>
         </ButtonRow>
         {/* row 3 */}
         <ButtonRow>
-        <Button onButtonPress={props.onButtonPress}>6</Button>
-        <Button onButtonPress={props.onButtonPress}>5</Button>
         <Button onButtonPress={props.onButtonPress}>4</Button>
+        <Button onButtonPress={props.onButtonPress}>5</Button>
+        <Button onButtonPress={props.onButtonPress}>6</Button>
         <Button onButtonPress={props.onButtonPress}>-</Button>
         </ButtonRow>
         {/* row 4 */}
         <ButtonRow>
-        <Button onButtonPress={props.onButtonPress}>3</Button>
-        <Button onButtonPress={props.onButtonPress}>2</Button>
         <Button onButtonPress={props.onButtonPress}>1</Button>
+        <Button onButtonPress={props.onButtonPress}>2</Button>
+        <Button onButtonPress={props.onButtonPress}>3</Button>
         <Button onButtonPress={props.onButtonPress}>+</Button>
         </ButtonRow>
         {/* row 5 */}

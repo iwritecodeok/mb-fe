32119461f3e87517ffd5a088fa21b582370c9cc5
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2020-08-07 14:42:08 -0500

    Update Banner.jsx

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Banner(){
     return(
         <div className = 'banner'>
-            <h1>Banner</h1>
+            <h1></h1>
         </div>
     )
 }
